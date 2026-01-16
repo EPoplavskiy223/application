d1b1d8b33f8004238aa067471fdb6efeb1bf48a6
Egor <epoplavskiy223@gmail.com>
2026-01-16 16:59:22 +0300

    done homework 23

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название", help_text='Введите название категории')
+    description = models.TextField(blank=True, null=True ,verbose_name='Описание категории', help_text='Введите описание категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название продукта", help_text='Введите название продукта')
+    description = models.TextField(blank=True, null=True ,verbose_name='Описание продукта', help_text='Введите описание продукта')
+    photo = models.ImageField(upload_to='photo', blank=True, null=True, verbose_name='Фото продукта', help_text='Загрузите фото продукта')
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name='products')
+    purchase_price = models.IntegerField(verbose_name='Цена за покупку продукта', help_text='Укажите цену за покупку продукта')
+    created_at = models.DateField(blank=True, null=True, verbose_name='Дата создания продукта', help_text='Укажите дату создания продукта')
+    updated_at = models.DateField(blank=True, null=True, verbose_name='Дата изменения продукта', help_text='Укажите дату изменения продукта')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['created_at', 'updated_at', 'name', 'category', 'purchase_price']
+
+    def __str__(self):
+        return self.name
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +79,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
@@ -119,3 +127,7 @@ STATICFILES_DIRS = (
     str(BASE_DIR / "static"),)
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
